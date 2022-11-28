c1b9f3f97279c19802b41935d211a2b90512b7d7
fabio.morooka <fabio.morooka@gmail.com>
2022-11-28 19:16:25 -0300

    Update cnn tests

--- cnn/models/convolutional_neural_network.py
+++ cnn/models/convolutional_neural_network.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+from numpy import size
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,27 +13,44 @@ class ConvolutionalNeuralNetwork(nn.Module):
         self.dropout = nn.Dropout(p_dropout)
             
         self.layer = nn.Sequential(
-          nn.Conv2d(self.input_size, 64, kernel_size = 3, padding = 1),
+          nn.Conv2d(self.input_size, 32, kernel_size = 3, padding = 1),
           nn.ReLU(),
-          nn.Conv2d(64,128, kernel_size = 3, stride = 1, padding = 1),
+          nn.Conv2d(32, 64, kernel_size = 3, padding = 1),
           nn.ReLU(),
-          nn.MaxPool2d(2,2),
+          nn.Conv2d(64, 64, kernel_size = 3, padding = 1),
+          nn.ReLU(),
+          nn.Conv2d(64, 128, kernel_size = 3, padding = 1),
+          nn.Dropout(0.25),
 
-          nn.Flatten(),
-          nn.Linear(2048, 1024),
+          nn.Conv2d(128, 128, kernel_size = 3, padding = 1),
           nn.ReLU(),
-          nn.Linear(1024, 512),
+          nn.Conv2d(128, 256, kernel_size = 3, padding = 1),
           nn.ReLU(),
-          nn.Linear(512, 256)          
+          nn.Conv2d(256, 256, kernel_size = 3, padding = 1),
+          nn.ReLU(),
+          nn.Conv2d(256, 512, kernel_size = 3, padding = 1),
+          nn.Dropout(0.25),
         )
 
-        self.relu = nn.ReLU()
+        self.flatten = nn.Sequential(
+          nn.AdaptiveMaxPool2d(2), 
+          nn.Flatten()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(2048, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Linear(256, self.output_size, bias=True, dtype=torch.double)
+        )
+
         self.output = nn.Linear(256, self.output_size, bias=True, dtype=torch.double)
 
     def forward(self, x):
         hidden = self.layer(x)
-        hidden = self.dropout(hidden)
-        hidden = torch.flatten(hidden, 1)
-        relu = self.relu(hidden)
-        output = self.output(relu)
+        #hidden = self.dropout(hidden)
+        hidden = self.flatten(hidden)
+        output = self.fc(hidden)
         return F.softmax(output, dim = 1)
